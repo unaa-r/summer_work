456557f6976749694bd9fee54a41ad3912182144
Noah Costa <n2costa@uwaterloo.ca>
2025-07-24 10:24:14 -0400

    archived results

--- two_interfaces/test.sh
+++ two_interfaces/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python -u interfaces_jul21.py --b 8300.0 --sigma_s 10.0 --dband 0.1 --pband 0.2 --output testjul22g
-# python -u interfaces_jul15.py --b 6640 --sigma_s 12.5 --output test_jul15b
+python -u interfaces_jul23.py --b 8300.0 --sigma_s 10.0 --dband 0.1 --pband 0.2 --output testjul23a
