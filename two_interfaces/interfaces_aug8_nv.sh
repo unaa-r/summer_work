b75c71706707241f3b8ad341a16ca624c65bc31d
Noah Costa <n2costa@uwaterloo.ca>
2025-08-08 16:38:42 -0400

    yet another typo

--- two_interfaces/interfaces_aug8_nv.sh
+++ two_interfaces/interfaces_aug8_nv.sh
@@ -10,12 +10,12 @@
 
 module load python
 
-line=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" params.txt)
+line=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" aug8params.txt)
 
 read A band <<< "$line"
 
 echo "Running with A=$A, bandwidth=$band"
 
-outfile = "two_interfaces_aug8_A${A}_band${band}"
+outfile="two_interfaces_aug8_A${A}_band${band}"
 
 srun python -u interfaces_aug6.py --b 8300.0 --sigma_s 10.0 --dband $band --pband 0.1 --output outfile --A $A
